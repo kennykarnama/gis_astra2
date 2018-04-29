451425784dd99a4f4d35980d464f0387875baa7d
kennykarnama <kennykarnama@gmail.com>
2018-04-29 13:38:29 +0700

    tidak jadi pakai job

--- app/Http/Controllers/Pages/ImportDataCustomerController.php
+++ app/Http/Controllers/Pages/ImportDataCustomerController.php
@@ -49,7 +49,73 @@ class ImportDataCustomerController extends Controller
 
               event(new ImportLaporanEvent("Data sedang diimport"));
 
-             dispatch(new ImportDataCustomerJob($path));
+                $nama_sheet = array();
+
+
+      $nama_sheet = Excel::load($path)->getSheetNames();
+
+            for($i=0; $i<count($nama_sheet); $i++){
+
+                //dd($nama_sheet[$i]);
+
+                Excel::selectSheets($nama_sheet[$i])->load($path, function($reader) {
+
+                    // Getting all results
+                    $results = $reader->get();
+
+                    $prepared_data = array();
+
+                    foreach($results as $row)
+                        {
+                            $headers = $row->keys()->all();
+
+                            //dd($headers);
+
+                            $tmp = array();
+
+                            $is_insert = 1;
+
+                            for($i = 0; $i < count($headers); $i++){
+                                
+                                $col_name = $headers[$i];
+
+                                $tmp[$col_name] = $row[$col_name];
+
+                                if(is_null($tmp[$col_name])){
+                                    $is_insert = 0;
+                                    break;
+                                }
+
+
+
+                            }
+
+                            $tmp['nama_sheet'] = $results->getTitle();
+
+                            //dd($tmp);
+
+                            if($is_insert == 1){
+
+                                 array_push($prepared_data, $tmp);
+
+                            }
+
+                           
+
+                        }
+
+                        //echo "ya"."<br>";
+
+                       DB::table('data_customer')->insert($prepared_data);
+
+                });
+            }
+
+            event(new ImportLaporanEvent("Data telah diimport"));
+
+
+
+             //dispatch(new ImportDataCustomerJob($path));
 
         
           
